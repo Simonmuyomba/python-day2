8bb87eda6237d6200e575d166fd5c9726f17752f
PETER MUYOMBA <122818012+Simonmuyomba@users.noreply.github.com>
2023-01-17 14:08:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Data Types#####################################
+#strings
+print("Hello"[0]) #Prints the first character
+print("Hello") #Prints the string
+
+#Integers
+print(123_432) #Prints the numbers
+print(123 + 345)
+
+#float
+print(123.456) #this is a float because it has a decimal point
+
+#Boolean
+True
+False
